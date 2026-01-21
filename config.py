09fa0034cc18d7b299a90f3f98e0c601041dc34e
Naman Pareek <144099870+pareeknaman@users.noreply.github.com>
2026-01-21 06:22:26 +0000

    Flatten repo: move all files to root and remove folder

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py - Database Configuration Template
+
+DB_CONFIG = {
+    "host": "localhost",
+    "port": 3306,
+    "username": "your_username",
+    "password": "your_password",
+    "database": "your_database_name"
+}
